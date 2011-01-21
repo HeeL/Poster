3b7dfb70a90284854bd652c0a543cbe49006d3b4
HeeL <parizhskiy@gmail.com>
2011-01-21 16:25:41 +0200

    ...

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,95 @@
+class UsersController < ApplicationController
+  before_filter :authenticate,   :except => [:show, :new, :create]
+  before_filter :correct_user,   :only => [:edit, :update]
+  before_filter :admin_user,     :only => :destroy
+  before_filter :signed_in_user, :only => [:new, :create]
+  
+  def index
+    @title = 'All users'
+    @users = User.paginate(:page => params[:page])
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(:page => params[:page])
+    @title = @user.name
+    
+  end
+
+  def new
+    @user = User.new
+    @title = "Sign up"
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      @user.password = nil
+      @user.password_confirmation = nil
+      render 'new'
+    end
+  end
+  
+  def edit
+    @title = "Edit user"
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    user_to_destroy = User.find(params[:id])
+    unless current_user?(user_to_destroy)
+      user_to_destroy.destroy
+      flash[:success] = "User destroyed."
+    else 
+      flash[:error] = "Can't destroy yourself!"
+    end
+    redirect_to users_path
+  end
+  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(:page => params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(:page => params[:page])
+    render 'show_follow'
+  end
+
+  private
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user 
+      redirect_to(root_path) unless current_user.admin?
+    end
+    
+    def signed_in_user
+      if signed_in?
+        flash[:error] = "Cannot create a new account when logged into an existing one."
+        redirect_to(root_path)
+      end
+    end 
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '7412dbde88abc3807236e30e2ae6b9c279d673ddea1ee6bc5e744582869073853e3fd52a71b3c3ac4235638ddb79f75fd794d7f52abf3d067f658e795538ec72'
